be72688eafec0343a4c0cb20728a4c5e30b0f0f5
Bill Merrill <evilai@gmail.com>
2014-04-06 14:11:04 -0700

    -save a mesh, write a file!

--- rip.py
+++ rip.py
@@ -9,6 +9,13 @@ PX = 0
 PY = 1
 PZ = 2
 
+TA = 0
+TB = 1
+TC = 2
+
+N = 0
+T = 1
+
 def get_general_info(dataset):
     print 'Driver: ', dataset.GetDriver().ShortName,'/', \
           dataset.GetDriver().LongName
@@ -58,8 +65,8 @@ def get_map(dataset):
 
 
 def compute_normal(triangle):
-    Nraw = numpy.cross( numpy.subtract(triangle['a'], triangle['b']),
-                               numpy.subtract(triangle['a'], triangle['c']) )
+    Nraw = numpy.cross( numpy.subtract(triangle[TA], triangle[TB]),
+                               numpy.subtract(triangle[TA], triangle[TC]) )
     hypo = math.sqrt(Nraw[PX]**2 + Nraw[PY]**2 + Nraw[PZ]**2)
     N = (Nraw[PX] / hypo,
          Nraw[PY] / hypo,
@@ -77,42 +84,67 @@ def write_triangle(sx,sy,triangle, normal):
     print("%s,%s\nN(%s, %s, %s) %s\na(%s, %s, %s), b(%s, %s, %s), c(%s, %s, %s)\n\n" % (
             sx,sy,
             normal[0], normal[1], normal[2], hyp(normal),
-            triangle['a'][PX], triangle['a'][PY], triangle['a'][PZ],
+            triangle[TA][PX], triangle[TA][PY], triangle[TA][PZ],
             triangle['b'][PX], triangle['b'][PY], triangle['b'][PZ],
             triangle['c'][PX], triangle['c'][PY], triangle['c'][PZ]))
 
-def s2n(dataset):
+def get_mesh(dataset):
     destination = "test-output-file.txt"
     band = dataset.GetRasterBand(1)
     emap = []
+    mesh = []
 
     for i in range(0, dataset.RasterYSize):
         scanline = band.ReadRaster( 0, i, band.XSize, 1, \
             band.XSize, 1, gdal.GDT_Float32 )
         emap.append(struct.unpack('f' * band.XSize, scanline))
 
-    print len(emap)
-    print len(emap[0])
-    print emap[10][20]
+    print "lines: %s" % len(emap)
+    print "cols: %s" % len(emap[0])
+    # print emap[10][20]
 
     def make_pt(x,y):
         return (float(x),float(y),float(emap[y][x]))
 
-    with open(destination, 'w') as dest_file:
-        for sy in range(0, dataset.RasterYSize-1):
-            for sx in range(0, dataset.RasterXSize-1):
-                triangle = {'a':make_pt(sx,sy),
-                            'b':make_pt(sx+1,sy+1),
-                            'c':make_pt(sx+1,sy)}
-                normal = compute_normal(triangle)
+    for sy in range(0, dataset.RasterYSize-1):
+        for sx in range(0, dataset.RasterXSize-1):
+            a_triangle = {TA:make_pt(sx,sy),
+                        TB:make_pt(sx+1,sy+1),
+                        TC:make_pt(sx+1,sy)}
+            a_normal = compute_normal(a_triangle)
+            b_triangle = {TA: make_pt(sx, sy),
+                          TB: make_pt(sx, sy+1),
+                          TC: make_pt(sx+1, sy+1)}
+            b_normal = compute_normal(b_triangle)
 
-                write_triangle(sx,sy,triangle, normal)
+            mesh.append((a_normal, a_triangle))
+            mesh.append((b_normal, b_triangle))
+            # write_triangle(sx,sy,triangle, normal)
+
+    print "triangles:  %s" % len(mesh)
+    return mesh
+
+def write_stl(outfile, mesh):
+    def write_header(f, mesh):
+        pass
+
+    with open(outfile, 'wb') as dest_file:
+        dest_file.write(struct.pack("80sI", b'Quick Release Lever', len(mesh)))
+        for f in mesh:
+            data = [f[N][PX], f[N][PY], f[N][PZ],
+                f[T][TA][PX], f[T][TA][PY], f[T][TA][PZ],
+                f[T][TB][PX], f[T][TB][PY], f[T][TB][PZ],
+                f[T][TC][PX], f[T][TC][PY], f[T][TC][PZ],0]
+            dest_file.write(struct.pack("12fH", *data))
+
 
 
 import pprint
 def main():
     dataset = gdal.Open('mto.tif', gdal.GA_ReadOnly)
-    s2n(dataset)
+    mesh = get_mesh(dataset)
+    print "writing"
+    write_stl("output.stl", mesh)
 # get_general_info(dataset)
 # emap = get_map(dataset)
 
