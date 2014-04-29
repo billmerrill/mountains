d0e704d31c074a47353b008b9a3fac4b5a9d825a
Bill Merrill <evilai@gmail.com>
2014-04-29 11:08:03 -0700

    add a base

--- rip.py
+++ rip.py
@@ -106,7 +106,7 @@ def get_mesh(dataset):
 
     pixel_size = get_pixel_meters(dataset)
     output_scalar = .1
-    sample_scale = 10
+    sample_scale = 100
 
     # multiple this vector again each point for the poly space
     pt_scalar = (sample_scale * pixel_size[PX] * output_scalar,
@@ -137,20 +137,24 @@ def get_mesh(dataset):
                 float(y) * pt_scalar[PY],
                 z)
 
-    # generate a mountain mesh
-    for sy in range(0, sample_height-1):
-        for sx in range(0, sample_width-1):
-            a_triangle = {TA:make_pt(sx,sy),
-                        TB:make_pt(sx+1,sy+1),
-                        TC:make_pt(sx+1,sy)}
-            a_normal = compute_normal(a_triangle)
-            b_triangle = {TA: make_pt(sx, sy),
-                          TB: make_pt(sx, sy+1),
-                          TC: make_pt(sx+1, sy+1)}
-            b_normal = compute_normal(b_triangle)
+    print "Scaled space option max is:"
+    pprint.pprint(make_pt(sample_width-1, sample_height-1, 0))
 
-            mesh.append((a_normal, a_triangle))
-            mesh.append((b_normal, b_triangle))
+    # generate a mountain mesh
+    if True:
+        for sy in range(0, sample_height-1):
+            for sx in range(0, sample_width-1):
+                a_triangle = {TA:make_pt(sx,sy),
+                            TB:make_pt(sx+1,sy+1),
+                            TC:make_pt(sx+1,sy)}
+                a_normal = compute_normal(a_triangle)
+                b_triangle = {TA: make_pt(sx, sy),
+                              TB: make_pt(sx, sy+1),
+                              TC: make_pt(sx+1, sy+1)}
+                b_normal = compute_normal(b_triangle)
+
+                mesh.append((a_normal, a_triangle))
+                mesh.append((b_normal, b_triangle))
 
 
     # generate a base box, bottom is at height 0 XXX TODO THESE NEED TO BE SCALED
@@ -229,6 +233,71 @@ def get_mesh(dataset):
 
         pprint.pprint(b_triangle)
         pprint.pprint(b_normal)
+
+    # real base
+
+    if True:
+        yhalf = sample_height / 2
+        xhalf = sample_width / 2
+        yquarter = sample_height / 4
+        xquarter = sample_width / 4
+
+        negx = make_pt(xquarter, yhalf, 0)
+        posx = make_pt(xquarter + xhalf, yhalf, 0)
+        negy = make_pt(xhalf, yquarter, 0)
+        posy = make_pt(xhalf, yquarter + yhalf, 0)
+
+        # star inset base
+        for sy in range(0, sample_height-1):
+            a_triangle = { TA: make_pt(0, sy, 0),
+                           TB: negx,
+                           TC: make_pt(0, sy+1, 0)}
+            z_triangle = { TA: make_pt(sample_width-1, sy, 0),
+                          TB: make_pt(sample_width-1, sy+1, 0),
+                          TC: posx }
+
+            mesh.append((compute_normal(a_triangle), a_triangle))
+            mesh.append((compute_normal(z_triangle), z_triangle))
+
+        for sx in range(0, sample_width-1):
+            a_triangle = { TA: make_pt(sx, 0, 0),
+                           TB: make_pt(sx+1, 0, 0),
+                           TC: negy }
+            z_triangle = { TA: make_pt(sx, sample_height-1, 0),
+                           TB: posy,
+                           TC: make_pt(sx+1, sample_height-1, 0) }
+
+            mesh.append((compute_normal(a_triangle), a_triangle))
+            mesh.append((compute_normal(z_triangle), z_triangle))
+
+
+        a_triangle = { TA: make_pt(0,0,0),
+                       TB: negy,
+                       TC: negx }
+        b_triangle = { TA: negy,
+                       TB: make_pt(sample_width-1, 0, 0),
+                       TC: posx }
+        c_triangle = { TA: posx,
+                       TB: make_pt(sample_width-1, sample_height-1, 0),
+                       TC: posy }
+        d_triangle = { TA: negx,
+                       TB: posy,
+                       TC: make_pt(0, sample_height-1, 0) }
+
+        e_triangle = { TA: negy,
+                       TB: posx,
+                       TC: negx }
+
+        f_triangle = { TA: posx,
+                       TB: posy,
+                       TC: negx }
+
+        mesh.append((compute_normal(a_triangle), a_triangle))
+        mesh.append((compute_normal(b_triangle), b_triangle))
+        mesh.append((compute_normal(c_triangle), c_triangle))
+        mesh.append((compute_normal(d_triangle), d_triangle))
+        mesh.append((compute_normal(e_triangle), e_triangle))
+        mesh.append((compute_normal(f_triangle), f_triangle))
 
 
     print "Generated %s triangles." % len(mesh)
